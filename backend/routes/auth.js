2a1efc9cd3e3fe209da2bab167967e6c9b6cbb86
anas-dew <mailtoanas@duck.com>
2022-10-10 13:04:13 +0530

    JWT added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 router.post('/', [
 
@@ -10,6 +12,7 @@ router.post('/', [
   body('password', 'Password must be atleast 7 characters long!').isLength({ min: 7 })
 ], async (req, res) => {
   let user = await User.findOne({ email: req.body.email })
+  
   if (user) {
     return res.status(400).json({ error: "Email already exists!" })
   }
@@ -17,14 +20,26 @@ router.post('/', [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPass =  await bcrypt.hash(req.body.password, salt)
+
   user = await User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPass,
   }).then(user => res.json('user add success..!')).catch(error => {
     console.error(error.message);
     res.send('Email already exists!')
   });
+
+  const data = {
+    user : {
+      id : user.id
+    }
+  }
+
+  const authToken = jwt.sign(data, 'lewpewmew');
 })
 
 
