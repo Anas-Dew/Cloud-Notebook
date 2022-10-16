ad47063dabf638d68a121e94839ee039c76c2679
anas-dew <mailtoanas@duck.com>
2022-10-16 09:59:49 +0530

    delete note endpoint added #54

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -21,7 +21,7 @@ router.post('/add-note', fetchuser, [
         return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const {title, description, tags} = req.body
+        const { title, description, tags } = req.body
         const note = new Notes({
             title, description, tags, user: req.user.id
         })
@@ -32,6 +32,39 @@ router.post('/add-note', fetchuser, [
     } catch (error) {
         return res.status(500).json({ error: "Internal Server Error" })
     }
+
+})
+// ROUTE : 3 -> UPDATE A NOTE FROM LOGGED IN USER (POST). LOGGED IN REQUIRED
+router.put('/update/:id', fetchuser, async (req, res) => {
+    const { title, description, tags } = req.body
+
+    const newNote = {};
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tags) { newNote.tags = tags }
+
+    let note = await Notes.findById(req.params.id) // params is for selecting from :id
+    if (!note) { return res.status(404).send('Not found') }
+
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send('Not Allowed');
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.send(note)
+
+})
+// ROUTE : 4 -> DELETE A NOTE (DELETE). LOGGED IN REQUIRED
+router.delete('/delete/:id', fetchuser, async (req, res) => {
+    
+
+    let note = await Notes.findById(req.params.id) // params is for selecting from :id
+    if (!note) { return res.status(404).send('Not found') }
+
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send('Not Allowed');
+    }
+    note = await Notes.findByIdAndDelete(req.params.id)
+    res.send('Delete Success.!')
 
 })
 module.exports = router
