578c370307044a39ac2755efd8a8f8023ca1b3a4
anas-dew <mailtoanas@duck.com>
2022-10-17 11:25:17 +0530

    react router setup #57

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import Account from './components/Account';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title='INoteBook' />
+
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/account" element={<Account/>} />
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
