cb9b97d1c9c6e3200f30ec12749427f09dfde233
anas-dew <mailtoanas@duck.com>
2022-11-17 15:41:15 +0530

    cors fixed

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 // const mongoURI = "mongodb://localhost:27017";
-const mongoURI = "mongodb+srv://public_user:me0IUpVVaY1PmvWV@copywordbase.pya1y.mongodb.net/?retryWrites=true&w=majority";
-// const mongoURI = `${process.env.MONGO_URI}`;
+const mongoURI = `${process.env.MONGO_URI}`;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
