2d3e55e729a2e0b52dec9c643f68c8fc34a6b619
anas-dew <mailtoanas@duck.com>
2022-11-17 15:30:32 +0530

    hard code url

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 // const mongoURI = "mongodb://localhost:27017";
-const mongoURI = `${process.env.MONGO_URI}`;
+const mongoURI = "mongodb+srv://public_user:me0IUpVVaY1PmvWV@copywordbase.pya1y.mongodb.net/?retryWrites=true&w=majority";
+// const mongoURI = `${process.env.MONGO_URI}`;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
